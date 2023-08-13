44bc5b32862bf26174bbdc0cf5395f76d59c3c58
holland-cw3 <Hollandcw3@gmail.com>
2023-08-13 14:59:43 -0400

    Mobile NavBar update, uses scrollmenu, and has animated hamburger bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,38 +9,31 @@ function Header() {
     const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
     useEffect(() => {
-        const listItems = document.querySelectorAll('.tab');
-        listItems.forEach((item) => {
-            item.classList.remove('active');
-        });
 
+        //removes active
+        const listItems = document.querySelectorAll('.tab');
+        listItems.forEach((item) => {item.classList.remove('active');});
+
+        //Adds active to current page on desktop
         const activeListItem = document.querySelector(`.tab a[href*="${location.pathname}"]`);
         if (activeListItem) {
             activeListItem.parentElement.classList.add('active');
         }
 
-        // Check if the current window width is less than or equal to 768px (typical mobile screen)
-        const handleResize = () => {
-            setIsMobileView(window.innerWidth <= 768);
-        };
-
-        // Add event listener to update isMobileView state on window resize
+        //for mobile
+        const handleResize = () => { setIsMobileView(window.innerWidth <= 768);};
         window.addEventListener('resize', handleResize);
-        // Call handleResize initially to set the initial isMobileView value
         handleResize();
 
-        // Cleanup the event listener on component unmount
         return () => window.removeEventListener('resize', handleResize);
     }, [location]);
 
-    const toggleMenu = () => {
-        setIsHamburgerOpen(!isHamburgerOpen);
-    };
+    const toggleMenu = () => { setIsHamburgerOpen(!isHamburgerOpen); };
 
     return (
         <header className="App-header bg-black">
-            {/* Display an image in place of the "UNIVERSITY OF MARYLAND CLUB RUNNING" tab on mobile */}
-            {isMobileView && (
+            
+            {isMobileView && (//title on mobile
                 <div className="MobileHome">
                     <a href="/home" className="home no-highlight text-xl text-red-600 font-bold whitespace-nowrap">
                         UMD CLUB RUNNING
@@ -48,96 +41,70 @@ function Header() {
                 </div>
             )}
 
-            {/* Hide the tab on mobile */}
             <Tab.Group className={`tabs ${isMobileView ? 'hidden' : ''}`}>
-                <Tab.List>
+                <Tab.List className={`text-white text-xl ${isMobileView ? 'hidden' : ''}`}>
                     <Tab className="tab home no-highlight text-xl text-red-600">
                         <a href="/home">UNIVERSITY OF MARYLAND CLUB RUNNING</a>
                     </Tab>
-                    <Tab.List className="text-white text-xl">
-                        <Tab className="tab" dropdown-toggleOff>
-                            <a href="https://gmail.us4.list-manage.com/subscribe/post?u=aea7db195a60f69f817e5110d&amp;id=c08d6f1486&amp;f_id=002edfe8f0">
-                                Sign-Up
-                            </a>
-                        </Tab>
-                        <Tab className="tab">
-                            <a href="/Blog">Blog</a>
-                        </Tab>
-                        <Tab className="tab">
-                            <a href="/Photos" className="dropdown-toggle">
-                                Archive
-                            </a>
-                            <ul className="dropdown">
-                                <li>
-                                    <a href="/Photos">Photos</a>
-                                </li>
-                                <li>
-                                    <a href="https://us4.campaign-archive.com/home/?u=aea7db195a60f69f817e5110d&amp;id=c08d6f1486">
-                                        Emails
-                                    </a>
-                                </li>
-                            </ul>
-                        </Tab>
-                        <Tab className="tab">
-                            <a href="/Races" className="dropdown-toggle">
-                                Info
-                            </a>
-                            <ul className="dropdown">
-                                <li>
-                                    <a href="/Races">Races</a>
-                                </li>
-                                <li>
-                                    <a href="/Records">Records</a>
-                                </li>
-                                <li>
-                                    <a href="/Workouts">Workouts</a>
-                                </li>
-                                <li>
-                                    <a href="/Routes">Routes</a>
-                                </li>
-                            </ul>
-                        </Tab>
-                        <Tab className="tab">
-                            <a href="/about" className="dropdown-toggle">
-                                About
-                            </a>
-                            <ul className="dropdown">
-                                <li>
-                                    <a href="/about">Officers</a>
-                                </li>
-                                <li>
-                                    <a href="/JoinUs">Join Us</a>
-                                </li>
-                            </ul>
-                        </Tab>
-                    </Tab.List>
+                    <Tab className="tab">
+                        <a href="https://gmail.us4.list-manage.com/subscribe/post?u=aea7db195a60f69f817e5110d&amp;id=c08d6f1486&amp;f_id=002edfe8f0">
+                            Sign-Up
+                        </a>
+                    </Tab>
+                    <Tab className="tab">
+                        <a href="/Blog">Blog</a>
+                    </Tab>
+                    <Tab className="tab">
+                        <a href="/Photos" className="dropdown-toggle">Archive</a>
+                        <ul className="dropdown">
+                            <li><a href="/Photos">Photos</a></li>
+                            <li><a href="https://us4.campaign-archive.com/home/?u=aea7db195a60f69f817e5110d&amp;id=c08d6f1486">Emails</a></li>
+                        </ul>
+                    </Tab>
+                    <Tab className="tab">
+                        <a href="/Races" className="dropdown-toggle">
+                            Info
+                        </a>
+                        <ul className="dropdown">
+                            <li><a href="/Races">Races</a></li>
+                            <li><a href="/Records">Records</a></li>
+                            <li><a href="/Workouts">Workouts</a></li>
+                            <li><a href="/Routes">Routes</a></li>
+                        </ul>
+                    </Tab>
+                    <Tab className="tab">
+                        <a href="/about" className="dropdown-toggle">About</a>
+                        <ul className="dropdown">
+                            <li><a href="/about">Officers</a></li>
+                            <li><a href="/JoinUs">Join Us</a></li>
+                        </ul>
+                    </Tab>
                 </Tab.List>
             </Tab.Group>
 
-            {/* Hamburger Icon */}
-            {isMobileView && (
-                <div className="header-wrapper flex justify-end">
-                    <div className="hamburger-menu mr-3 mt-1" onClick={toggleMenu}>
-                        <span className="hamburger-icon text-3xl" style={{ color: 'white' }}>
-                            &#9776;
-                        </span>
-                        {isHamburgerOpen && (
-                            <div className="hamburger-content text-white">
-                                <ul>
-                                    <li>
-                                        <a href="/home">Home</a>
-                                    </li>
-                                    <li>
-                                        <a href="/gallery">Gallery</a>
-                                    </li>
-                                    <li>
-                                        <a href="/contact">Contact</a>
-                                    </li>
-                                    {/* Add more menu items as needed */}
-                                </ul>
-                            </div>
-                        )}
+            {isMobileView && ( //hamburger menu
+                <>
+                    <div className="header-wrapper flex justify-end">
+                        <div className={`hamburger-menu mr-5 -mt-2 mb-1 ${isHamburgerOpen ? 'change' : ''}`} onClick={toggleMenu}>
+                            <div className="bar1 bg-white w-6 h-[5px]"></div>
+                            <div className="bar2 bg-white w-6 h-[5px]"></div>
+                            <div className="bar3 bg-white w-6 h-[5px]"></div>
+                        </div>
                     </div>
+                </>
+            )}
+
+            {isMobileView && isHamburgerOpen && ( //scrollable menu on click
+                <div className="scrollmenu font-bold text-white">
+                    <a href="https://gmail.us4.list-manage.com/subscribe/post?u=aea7db195a60f69f817e5110d&amp;id=c08d6f1486&amp;f_id=002edfe8f0">Sign-Up</a>
+                    <a href="/Blog">Blog</a>
+                    <a href="/Photos">Photos</a>
+                    <a href="/Races">Races</a>
+                    <a href="/Records">Records</a>
+                    <a href="/Workouts">Workouts</a>
+                    <a href="/Routes">Routes</a>
+                    <a href="/about">Officers</a>
+                    <a href="https://docs.google.com/document/d/1AbWsGq4yMjutG-I_jPUq9fuutEBvtcoPZ5IwTnRc1Ns/edit">Join Us</a>
                 </div>
             )}
         </header>
--- src/pages/JoinUs.js
+++ src/pages/JoinUs.js
@@ -1,22 +1,7 @@
-import React from 'react';
-import { Tab } from '@headlessui/react'
+
 
 function Routes() {
 
-    return (
-        <Tab.Group>
-            <Tab.List>
-                <Tab>Tab 1</Tab>
-                <Tab>Tab 2</Tab>
-                <Tab>Tab 3</Tab>
-            </Tab.List>
-            <Tab.Panels>
-                <Tab.Panel>Content 1</Tab.Panel>
-                <Tab.Panel>Content 2</Tab.Panel>
-                <Tab.Panel>Content 3</Tab.Panel>
-            </Tab.Panels>
-        </Tab.Group>
-    )
 }
 
 export default Routes;
