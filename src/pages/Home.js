0c4f9ec818faa0bfbb4063a645b27ee5220c3b4b
holland-cw3 <Hollandcw3@gmail.com>
2023-08-17 15:34:03 -0400

    homePage almost complete, need to add sign-up for emails, change carousel descriptions, and change images in the body content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,6 @@ import natsPic from '../Pictures/natsTeamPic.JPG';
 import lillianCapital from '../Pictures/lillianCapital.JPG';
 import fredHalfPic from '../Pictures/fredHalfTeam.JPG';
 
-
-
 function Home() {
 
     const images = [
@@ -35,20 +33,42 @@ function Home() {
                 <ImageGallery items={images} renderItem={renderCustomImageItem} showFullscreenButton={false} showPlayButton={false} autoPlay={true} slideInterval={5000}/>   
             </div>
 
-            <section id="main" className="flex justify-center">
-                <div className="rounded-xl border-2 border-white mt-10 mb-10 w-11/12 bg-white bg-opacity-70">
-                    <h1 className="text-black font-bold text-2xl mb-4 mt-5 underline">About Us</h1>
-                    <p className="text-black mb-2">
-                        Please sign up to join us Mon-Fri at the <a href="https://www.google.com/maps/place/PP1,+College+Park,+MD+20740/@38.9933786,-76.9425631,148m/data=!3m2!1e3!4b1!4m2!3m1!1s0x89b7c69f4dc17645:0xd2d09f5ca7e6b0d9" className="text-blue-700 font-bold">School of Public Health (SPH)</a> East Side (closest to Xfinity Center) under the overhang starting<b> January 30th. Practices now start at 5pm! </b>
-                    </p>
-
-                    <p p className="text-black mb-7">
-                        Our mission is to support and encourage a healthy approach to running and training for all members of the University of Maryland. We provide an atmosphere in which runners of all abilities can meet each other, share running knowledge and simply enjoy running together as well as compete against other club college teams.
-                    </p>
-                    <a href="https://docs.google.com/document/d/1AbWsGq4yMjutG-I_jPUq9fuutEBvtcoPZ5IwTnRc1Ns/edit#heading=h.vkmalmlatt01"><button class="button"><span>Join Us! </span></button></a>
+            <section id="main" className="flex flex-col items-center md:flex-row justify-center">
+                <div className="rounded-xl border-2 border-white mt-10 mb-10 w-11/12 bg-white bg-opacity-70 flex flex-col md:flex-row">
+                    <div className="align-left md:w-1/2">
+                        <img src={natsPic} alt="Team Pic" className="rounded-xl" />
+                    </div>
+                    <div className="flex flex-col md:w-1/2 ml-5 mr-5">
+                        <h1 className="text-black font-bold text-3xl mb-4 mt-5 md:mt-20 underline">About Us</h1>
+                        <p className="text-black mb-6">
+                            Please sign up to join us Mon-Fri at the <a href="https://www.google.com/maps/place/PP1,+College+Park,+MD+20740/@38.9933786,-76.9425631,148m/data=!3m2!1e3!4b1!4m2!3m1!1s0x89b7c69f4dc17645:0xd2d09f5ca7e6b0d9" className="text-blue-700 font-bold">School of Public Health (SPH)</a> East Side (closest to Xfinity Center) starting <b>January 30th. Practices now start at 5pm! </b>
+                        </p>
+                        <p className="text-black mb-7">
+                            Our mission is to support and encourage a healthy approach to running and training for all members of the University of Maryland. We provide an atmosphere in which runners of all abilities can meet each other, share running knowledge and simply enjoy running together as well as compete against other club college teams.
+                        </p>
+                        <a href="https://docs.google.com/document/d/1AbWsGq4yMjutG-I_jPUq9fuutEBvtcoPZ5IwTnRc1Ns/edit#heading=h.vkmalmlatt01">
+                            <button className="button"><span>Join Us!</span></button>
+                        </a>
+                    </div>
                 </div>
             </section>
 
+            <section id="main" className="flex flex-col items-center md:flex-row justify-center">
+                <div className="rounded-xl border-2 border-white mt-10 mb-10 w-11/12 bg-white bg-opacity-70 flex flex-col md:flex-row">
+                    <div className="align-left md:w-1/2">
+                        <img src={natsPic} alt="Team Pic" className="rounded-xl" />
+                    </div>
+                    <div className="flex flex-col md:w-1/2 ml-5 mr-5">
+                        <h1 className="text-black font-bold text-3xl mb-4 mt-5 md:mt-20 underline">Weekly Newsletter</h1>
+                        <p className="text-black mb-6">
+                            Stay in the loop! Subscribe to our weekly mailing list to learn all about our upcoming social events, practices, races, and more! Whether you're a seasoned runner or just starting out, being part of our listserv is the best way to stay informed and engaged. Don't miss out on the latest opportunities to run and connect with fellow club runners!                        </p>
+                        
+                        <a href="https://docs.google.com/document/d/1AbWsGq4yMjutG-I_jPUq9fuutEBvtcoPZ5IwTnRc1Ns/edit#heading=h.vkmalmlatt01">
+                            <button className="button"><span>Sign-Up!</span></button>
+                        </a>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
