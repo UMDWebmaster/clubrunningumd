be39e06bde7df9bc4ebcfef15fedb76845611884
holland-cw3 <Hollandcw3@gmail.com>
2023-08-23 15:42:12 -0400

    signup retry

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -32,44 +32,36 @@ function Records() {
 
     return (
         <div className="construc-container">
-            <link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css"/>
-                <div id="mc_embed_signup">
-                <form action="https://gmail.us4.list-manage.com/subscribe/post?u=aea7db195a60f69f817e5110d&amp;id=c08d6f1486&amp;f_id=002edfe8f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                        <div id="mc_embed_signup_scroll">
-                        <h2>Maryland Club Running Email List</h2>
+            <div id="mc_embed_shell">
+                <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css"/>
+                
+                    <div id="mc_embed_signup">
+                        <form action="https://gmail.us4.list-manage.com/subscribe/post?u=aea7db195a60f69f817e5110d&amp;id=c08d6f1486&amp;f_id=002edfe8f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                            <div id="mc_embed_signup_scroll"><h2>Maryland Club Running 2023-2024 Email List</h2>
+                                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                            <div class="mc-field-group">
+                                <label for="mce-EMAIL">Email Address2 <span class="asterisk">*</span></label>
+                                <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" /></div>
 
-                            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-                            <div class="mc-field-group">
-                                <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-                                </label>
-                                <input type="email" name="EMAIL" class="required email" id="mce-EMAIL"/>
-                                    <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-                            </div>
-                            <div class="mc-field-group">
-                                <label for="mce-FNAME">First Name </label>
-                                <input type="text" name="FNAME" class="" id="mce-FNAME"/>
-                                    <span id="mce-FNAME-HELPERTEXT" class="helper_text"></span>
-                            </div>
-                            <div class="mc-field-group">
-                                <label for="mce-LNAME">Last Name </label>
-                                <input type="text" name="LNAME" class="" id="mce-LNAME"/>
-                                    <span id="mce-LNAME-HELPERTEXT" class="helper_text"></span>
-                            </div>
-                            <div><input type="hidden" name="tags" value="7025981"/></div>
-                            <div id="mce-responses" class="clear foot">
-                                <div class="response" id="mce-error-response"></div>
-                                <div class="response" id="mce-success-response"></div>
-                            </div>    
-                            <div class="optionalParent">
-                                <div class="clear foot">
-                                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/>
-                                        <p class="brandingLogo"><a href="http://eepurl.com/h8EvGH" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" alt="mail"/></a></p>
+                            <div class="mc-field-group"><label for="mce-FNAME">First Name </label><input type="text" name="FNAME" class=" text" id="mce-FNAME"  /></div>
+                            <div class="mc-field-group"><label for="mce-LNAME">Last Name </label><input type="text" name="LNAME" class=" text" id="mce-LNAME"  /></div>
+                                <div hidden=""><input type="hidden" name="tags" value="7835950"/></div>
+                                <div id="mce-responses" class="clear foot">
+                                    <div class="response" id="mce-error-response"></div>
+                                    <div class="response" id="mce-success-response"></div>
+                                </div>
+                                
+                                <div class="optionalParent">
+                                    <div class="clear foot">
+                                        <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe"/>
+                                            <p><a href="http://eepurl.com/h8EvGH" title="Mailchimp - email marketing made easy and fun"><span><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" /></span></a></p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
-                  
-                </div>
+                        </form>
+                    </div>
+
+            </div>
         </div>
     );
 }
