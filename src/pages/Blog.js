905972d314aaa2503b34f4595c629897f508244b
Evan <evanmasiello@gmail.com>
2023-12-08 21:24:27 -0500

    added link to new blog on blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -11,7 +11,7 @@ function Records() {
                 <script>window.location.replace("https://evanmasiello.com/clubrunning/");</script>
                 <h1>Under Construction</h1>
                 <img src={construcPic} alt="Under Construction" className="construction-img" />
-                <p>We are currently working on something awesome. Stay tuned! <a href="https://evanmasiello.com/clubrunning/">You can find the new blog here!</a></p>
+                <p color="blue"><strong><a href="https://evanmasiello.com/clubrunning/">You can find the new blog here!</a></strong></p>
             </main>
         </div>
     );
