9d432ff9bbe299a31b54681857b5ff796ea221e1
evan <evanmasiello@gmail.com>
2024-01-02 16:39:49 -0500

    Added more routes and a form for route submission

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -17,7 +17,7 @@ function Routes() {
                     <table className="mb-10">
                         <thead>
                             <tr>
-                                <th>Routes (5-7 Miles)</th>
+                                <th>Routes (4-7 Miles)</th>
                                 <th>Distance</th>
                                 <th>Routes (7+ Miles)</th>
                                 <th>Distance</th>
@@ -25,87 +25,117 @@ function Routes() {
                         </thead>
                         <tbody>
                             <tr>
-                                <td><a href="http://www.mapmyrun.com/routes/view/1391591584">Lake Loop</a></td>
-                                <td>4.61 Miles</td>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1821760106">Mikey T's</a></td>
-                                <td>8.5 miles (maybe)</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/5868853243">Hamster Cage V2</a></td>
+                                <td>4.45 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/5868922576">Hills 4 Redacted</a></td>
+                                <td>7.09 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="http://www.mapmyrun.com/routes/view/1391575675">Cherry Chill</a></td>
-                                <td>5.10 Miles</td>
+                                <td><a href="http://www.mapmyrun.com/routes/view/1391591584">Lake Loop</a></td>
+                                <td>4.61 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1821774887">Brigg's Beltway Bash</a></td>
                                 <td>7.47 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1403709331">Neighborhoods</a></td>
-                                <td>5.36 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/2289721822">Historic Berwyn</a></td>
+                                <td>4.76 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1391609449">Riverchill</a></td>
                                 <td>7.65 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1875418334">PG Plaza</a></td>
-                                <td>6.98 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/2289717640">Cherry Chill Loop</a></td>
+                                <td>4.88 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/2289720103">Greenbelt Metro</a></td>
                                 <td>7.85 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2289717640">Cherry Chill Loop</a></td>
-                                <td>4.88 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/5868848695">Hamster Cage V1</a></td>
+                                <td>4.92 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1403696812">Hyattschill</a></td>
                                 <td>8.09 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2289721822">Historic Berwyn</a></td>
-                                <td>4.76 Miles</td>
+                                <td><a href="http://www.mapmyrun.com/routes/view/1391575675">Cherry Chill</a></td>
+                                <td>5.10 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1821760106">Mikey T's</a></td>
+                                <td>8.5 miles (maybe)</td>
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1403709331">Neighborhoods</a></td>
+                                <td>5.36 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1875414515">Lost</a></td>
                                 <td>8.53 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2648473711">Hills 4 Days</a></td>
-                                <td>5.85 Miles</td>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1391636146">Berwyn</a></td>
-                                <td>9.84 Miles</td>
-                            </tr>
-                            <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2289735244">Neighborhoods V2</a></td>
-                                <td>6.35 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1875419546">Hammock Park</a></td>
+                                <td>5.39 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1391628139">North Northwest</a></td>
                                 <td>9.05 Miles</td>
                             </tr>
                             <tr>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1875411272">Communism</a></td>
                                 <td>5.72 Miles</td>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1403705587">South Northwest</a></td>
-                                <td>11.45 Miles</td>
-                            </tr>
-                            <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1875416639">Duck Pond</a></td>
-                                <td>6.55 Miles</td>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2289727930">Cemetery</a></td>
-                                <td>9.89 Miles</td>
-                            </tr>
-                            <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1875419546">Hammock Park</a></td>
-                                <td>5.39 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/2289731311">Greenbelt Lake</a></td>
                                 <td>9.50 Miles</td>
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/5869010728">Trolley Trail / Farmers Market</a></td>
+                                <td>6.04 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1391636146">Berwyn</a></td>
+                                <td>9.84 Miles</td>
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1391623825">Metro</a></td>
+                                <td>6.06 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/2289727930">Cemetery</a></td>
+                                <td>9.89 Miles</td>
                             </tr>   
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2648461723">PG Plaza to Riverdale</a></td>
-                                <td>6.86 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/2648473711">Hills 4 Days V2</a></td>
+                                <td>6.07 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/2289733390">Sligo</a></td>
                                 <td>11.10 Miles</td>
                             </tr>  
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1391623825">Metro</a></td>
-                                <td>6.06 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/5868878107">Old Hills 4 Days</a></td>
+                                <td>6.07 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1821782096">Cherry Kill</a></td>
                                 <td>11.23 Miles</td>
-                            </tr>                                                                                                   
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/2289735244">Neighborhoods V2</a></td>
+                                <td>6.35 Miles</td>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1403705587">South Northwest</a></td>
+                                <td>11.45 Miles</td>
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/5868859933">CP Woods</a></td>
+                                <td>6.39 Miles</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1875416639">Duck Pond</a></td>
+                                <td>6.55 Miles</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/2648461723">PG Plaza to Riverdale</a></td>
+                                <td>6.86 Miles</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td><a href="https://www.mapmyrun.com/routes/view/1875418334">PG Plaza</a></td>
+                                <td>6.98 Miles</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
                         </tbody>
-                       
-                       
                     </table>
+                    <h1>If a route is missing please use <strong><a class="link" href="https://forms.gle/FvunMkBVNLjz65XL8" target="_blank">this form</a></strong> to submit it!</h1>
+                    <br></br>
                 </div>
             </div>
         </div>
