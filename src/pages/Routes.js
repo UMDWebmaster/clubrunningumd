b7320c5931bc9b2ecf398fceea24877c82ef77f2
evan <evanmasiello@gmail.com>
2024-01-03 11:24:33 -0500

    added a new route

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -79,49 +79,55 @@ function Routes() {
                                 <td>9.50 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/5869010728">Trolley Trail / Farmers Market</a></td>
-                                <td>6.04 Miles</td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/5869893229/">Shorter Metro</a></td>
+                                <td>5.72 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1391636146">Berwyn</a></td>
                                 <td>9.84 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1391623825">Metro</a></td>
-                                <td>6.06 Miles</td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/5869010728">Trolley Trail / Farmers Market</a></td>
+                                <td>6.04 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/2289727930">Cemetery</a></td>
                                 <td>9.89 Miles</td>
                             </tr>   
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2648473711">Old Hills 4 Days</a></td>
-                                <td>6.07 Miles</td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/1391623825">Metro</a></td>
+                                <td>6.06 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/2289733390">Sligo</a></td>
                                 <td>11.10 Miles</td>
                             </tr>  
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/5868878107">Hills 4 Days V2</a></td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/2648473711">Old Hills 4 Days</a></td>
                                 <td>6.07 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1821782096">Cherry Kill</a></td>
                                 <td>11.23 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2289735244">Neighborhoods V2</a></td>
-                                <td>6.35 Miles</td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/5868878107">Hills 4 Days V2</a></td>
+                                <td>6.07 Miles</td>
                                 <td><a href="https://www.mapmyrun.com/routes/view/1403705587">South Northwest</a></td>
                                 <td>11.45 Miles</td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/5868859933">CP Woods</a></td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/2289735244">Neighborhoods V2</a></td>
+                                <td>6.35 Miles</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                            <td><a href="https://www.mapmyrun.com/routes/view/5868859933">CP Woods</a></td>
                                 <td>6.39 Miles</td>
                                 <td></td>
                                 <td></td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/1875416639">Duck Pond</a></td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/1875416639">Duck Pond</a></td>
                                 <td>6.55 Miles</td>
                                 <td></td>
                                 <td></td>
                             </tr>
                             <tr>
-                                <td><a href="https://www.mapmyrun.com/routes/view/2648461723">PG Plaza to Riverdale</a></td>
+                            <td><a href="https://www.mapmyrun.com/routes/view/2648461723">PG Plaza to Riverdale</a></td>
                                 <td>6.86 Miles</td>
                                 <td></td>
                                 <td></td>
