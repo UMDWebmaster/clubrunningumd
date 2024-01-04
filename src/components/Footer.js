163f349e45d6b2a165b1e50eda1f4a5b57aac335
evan <evanmasiello@gmail.com>
2024-01-03 20:35:31 -0500

    added email to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaStrava, FaEnvelope } from 'react-
 function Footer() {
     return (
         <>
-            <footer id="footer" className="bg-neutral-900 flex items-center justify-center border-t border-white border-opacity-70 text-white py-6 w-full">
+            <footer id="footer" className="bg-neutral-900 flex items-center justify-center border-t border-white border-opacity-70 text-white pt-6 pb-1 w-full">
                 <div className="inner">
                     <ul className="flex color-white">
                         <li className="inline-block mr-5 color-white"><a href="https://www.facebook.com/UMDclubrunning/?fref=ts"><FaFacebook size={32}/></a></li>
@@ -12,7 +12,14 @@ function Footer() {
                         <li className="inline-block mr-5"><a href="https://www.instagram.com/umdclubrunning/"><FaInstagram size={32} /></a></li>
                         <li className="inline-block mr-5"><a href="mailto:umdclubrunning@gmail.com"><FaEnvelope size={32} /></a></li>
                         <li className="inline-block"><a className="icon" href="https://www.strava.com/clubs/874154"><FaStrava size={32} /></a></li>
-                    </ul>                  
+                    </ul>
+                </div>
+            </footer>
+            <footer className="bg-neutral-900 flex items-center justify-center text-white pt-2 pb-3 w-full">
+                <div className="inner">
+                    <ul className="flex color-white">
+                        <li className="inline-block mr-5 color-white">Report bugs or request features to <strong><a href="mailto:umdwebmast3r@gmail.com">umdwebmast3r@gmail.com</a></strong></li>
+                    </ul>
                 </div>
             </footer>
         </>
