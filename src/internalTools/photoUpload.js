f36489712f65ede3eec6ed0cfa6690716c19aa30
holland-cw3 <Hollandcw3@gmail.com>
2024-07-07 17:22:02 -0500

    hii

--- src/internalTools/photoUpload.js
+++ src/internalTools/photoUpload.js
@@ -2,17 +2,34 @@ import { Container } from "@mui/material";
 import React from "react";
 
 
+function swap(s) {
+
+  let arr = s.split('');
+  // Swap adjacent digits if they have the same parity
+  for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1; j++) {
+        let num1 = parseInt(arr[j], 10);
+        let num2 = parseInt(arr[j + 1], 10);
+          if (num1 % 2 === num2 % 2 && num1 < num2) {
+              [num1, num2] = [num2, num1];
+          }
+      }
+  }
+  arr.join('');
+return arr;
+}
+
 // Define a functional component named Races
 function photoUpload() {
-  
+
+
+
+
 
   return (
     <>
       <Container className="bg-white">
-        <ul>
-        <li>hi</li>
-          <li>hi</li>
-        </ul>
+        {swap(s)}
       </Container>
     </>
       
